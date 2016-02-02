464308fcf3a3d89f7ca589c0e4b14da7cd5ddb8d
Paul Bailey <paul.bailey@rinbo.consulting>
2016-02-02 13:43:48 +0000

    Removing version number For Reasons

--- dvdid.rb
+++ dvdid.rb
@@ -4,7 +4,6 @@ class Dvdid < Formula
   homepage "http://dvdid.cjkey.org.uk"
   url "http://dvdid.cjkey.org.uk/dvdid-0.1.1.tar.gz"
   sha256 "2f08e88c8f84bb44ffcb8746bff14ab3d89300d9c12206d03bf0d88408e27d74"
-  version "0.1.1"
 
   def install
     system "./configure", "--prefix=#{prefix}"

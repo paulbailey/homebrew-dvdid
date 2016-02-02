9d884056474dd1e56dc6cef8806f1a340feb249f
Paul Bailey <paul.bailey@rinbo.consulting>
2016-02-02 13:38:10 +0000

    Moved back

--- dvdid.rb
+++ dvdid.rb
@@ -1,7 +1,10 @@
+require "formula"
+
 class DvdId < Formula
   homepage "http://dvdid.cjkey.org.uk"
   url "http://dvdid.cjkey.org.uk/dvdid-0.1.1.tar.gz"
   sha256 "2f08e88c8f84bb44ffcb8746bff14ab3d89300d9c12206d03bf0d88408e27d74"
+  version "0.1.1"
 
   def install
     system "./configure", "--prefix=#{prefix}"
